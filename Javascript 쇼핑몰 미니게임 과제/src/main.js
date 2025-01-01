c3ed182abce309ac15a6827afe29b239a433e851
SuY3450 <suyeon030609@gmail.com>
2025-01-01 14:25:06 +0900

    JavaScript - fetch

---
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -0,0 +1,18 @@
+
+//Fetch the items from th JSON file
+function loadItems() {
+    return fetch('data/data.json')
+        .then(response => response.json())
+        .then(json => json.items);
+}
+
+
+
+//main
+loadItems()
+    .then(items => {
+        console.log(items);
+        // displayItems(items);
+        // setEventListners(items)
+    })
+.catch(console.log);

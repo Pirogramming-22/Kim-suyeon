1b758561e15eef40e018805926e2ff6b3c4b28a5
SuY3450 <suyeon030609@gmail.com>
2025-01-03 04:04:47 +0900

    python_problem_1__8단계

--- ptyhon_problem/python_problem_1.py
+++ ptyhon_problem/python_problem_1.py
@@ -1,42 +1,25 @@
-#1단계: 변수 num 선언
+#8단계
+def brGame(player_name, opponent_name):
+    global num
+    while True:
+        inputCount = input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : ")
+        try:
+            numberCount = int(inputCount)
+            if numberCount not in [1,2,3]:
+                print("1,2,3 중 하나를 입력하세요")
+            else:
+                for i in range(numberCount):
+                    num+=1
+                    print("{0} : {1}".format(player_name,num))
+                    if num == 31:
+                        print("{0} win!".format(opponent_name)) 
+                        exit()
+                break
+        except ValueError:
+            print("정수를 입력하세요")
+
+
 num = 0
-
-#6단계
 while num < 31:
-    #3단계
-    while True:
-        #2단계: input()함수 이용
-        inputCount = input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : ")
-        try:
-            numberCount = int(inputCount)
-            if numberCount not in [1,2,3]:
-                print("1,2,3 중 하나를 입력하세요")
-            else:
-                #4단계
-                for i in range(numberCount):
-                    num+=1
-                    print("playerA : {0}".format(num))
-                    if num == 31:
-                        print("playerB win!") #7단계
-                        exit()
-                break
-        except ValueError:
-            print("정수를 입력하세요")
-
-    #5단계
-    while True:
-        inputCount = input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : ")
-        try:
-            numberCount = int(inputCount)
-            if numberCount not in [1,2,3]:
-                print("1,2,3 중 하나를 입력하세요")
-            else:
-                for i in range(numberCount):
-                    num+=1
-                    print("playerB : {0}".format(num))
-                    if num == 31:
-                        print("playerA win!") #7단계
-                        exit()
-                break
-        except ValueError:
-            print("정수를 입력하세요")
+    brGame("playerA", "playerB")
+    brGame("playerB","playerA")
